8019c940dcb1c934aafc71d691a7ceadc6458bf3
Oscar Casagrande <oscar.casagrande@hotmail.com.br>
2018-01-02 10:37:58 -0200

    teste com threads

--- Thor/Program.cs
+++ Thor/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace Thor
 {
@@ -10,6 +12,25 @@ namespace Thor
     {
         static void Main(string[] args)
         {
+            Thread t = new Thread(NovaThread);
+            Thread t2 = new Thread(() => MaisUmaNovaThread(100));
+            t.Start();
+            t2.Start();
+            
+
+            for (int i = 0; i < 100; i++) Console.Write("1");
+
+            Console.Read();
+        }
+
+        static void NovaThread()
+        {
+            for (int i = 0; i < 100; i++) Console.Write("2");
+        }
+
+        static void MaisUmaNovaThread(int x)
+        {
+            for (int i = 0; i < x; i++) Console.Write("3");
         }
     }
 }
